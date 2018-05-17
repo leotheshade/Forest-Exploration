521f4a11608db4b144b071f5c4b7e1f2af7a6fa9
leotheshade <leotheshade@gmail.com>
2018-05-17 06:23:59 -0700

    made movement work

--- code.js
+++ code.js
@@ -5,14 +5,26 @@ var VTX = parseFloat(0)
 var VTY = parseFloat(0)
 var VBX = parseFloat(750)
 var VBY = parseFloat(500)
+
+var PX = parseFloat(325)
+var PY = parseFloat(220)
+
+var PUYC = 0
+var PRXC = 0
+var PDYC = 0
+var PLXC = 0
+
 var UYC = 0
 var RXC = 0
 var DYC = 0
 var LXC = 0
 var ZO = 0
 var ZI = 0
+
+
 UpdatePos()
 function UpdatePos(){
+
   if(RXC == 1){
     VTX += 2
   }
@@ -33,79 +45,110 @@ function UpdatePos(){
    VBX -= 2
       VBY -= 2
   }
-  
+//player down
+  if(PRXC == 1){
+    PX += 2
+  }
+   if(PUYC == 1){
+     PY -= 2
+  }
+   if(PLXC == 1){
+     PX -= 2
+  }
+   if(PDYC == 1){
+  PY += 2
+  }
+
   document.getElementById("canvas").setAttribute("viewBox", "" +VTX + " " + VTY +" "+ VBX +" "+ VBY);
+  document.getElementById("player").setAttribute("x", px)
+  document.getElementById("player").setAttribute("y", py)
+
   requestAnimationFrame(UpdatePos)
-  
+
 }
 function stopM(event){
   if(event.key == "z"){
     ZI = 0
-    
+
   }
   if(event.key == "x"){
     ZO = 0
   }
   if(event.key == "w" ){
     UYC = 0
+    PUYC = 0
+
     // VTY -= 1
     // VBY += 1
   }
-  
+
   if(event.key == "s"){
     DYC = 0
+    PDYC = 0
+
    // VTY += 1
-   
-    
+
+
   }
   if(event.key == "d"){
     RXC = 0
+    PRXC = 0
+
    // VTX += 1
-   
-    
+
+
   }
   if(event.key == "a"){
    LXC = 0
+   PLXC = 0
+
    // VTX -= 1
-   
-    
+
+
   }
-  
+
 }
 
 function moveC(event){
   console.log("hu")
   if(event.key == "z"){
     ZI = 1
-    
+
   }
   if(event.key == "x"){
     ZO = 1
   }
   if(event.key == "w" ){
     UYC = 1
+    PUYC = 1
+
     // VTY -= 1
     // VBY += 1
   }
-  
+
   if(event.key == "s"){
     DYC = 1
+    PDYC = 1
+
    // VTY += 1
-   
-    
+
+
   }
   if(event.key == "d"){
     RXC = 1
+    PRXC = 1
+
    // VTX += 1
-   
-    
+
+
   }
   if(event.key == "a"){
-   LXC = 1 
-   // VTX -= 1
-   
-    
-  }
-  
-}
+   LXC = 1
+   PLXC = 1
 
+   // VTX -= 1
+
+
+  }
+
+}
